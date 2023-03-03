a1c843a8c3d05a9927da77072dd9580c68861915
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2023-03-03 16:01:19 +0500

    add out_folder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, argparse, qrcode
+import sys, argparse, qrcode, os
 from qrcode.image.pure import PyPNGImage
 
 from gettext import gettext as _
@@ -19,7 +19,9 @@ def read_file(file: TXTFileType) -> list:
     return lines
 
 
-def qr_code_generator(file: TXTFileType, box_size: int, border: int, image_factory):
+def qr_code_generator(
+    file: TXTFileType, box_size: int, border: int, image_factory, out_folder
+):
     if box_size <= 0 and border <= 0:
         raise ValueError(
             f"box_size va border 0 dan kichik qiymat qabul qila olmaydi: {box_size}, {border}"
@@ -30,6 +32,9 @@ def qr_code_generator(file: TXTFileType, box_size: int, border: int, image_facto
         raise ValueError(f"border 0 dan kichik qiymat qabul qila olmaydi: {border}")
 
     lines = read_file(file)
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+
     for line in lines:
         qr = qrcode.QRCode(box_size=box_size, border=border)
         qr.add_data(line, 0)
@@ -55,9 +60,12 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("--box_size", default=10, type=int, help="")
     parser.add_argument("--border", default=4, type=int, help="")
     parser.add_argument("--image_factory", default=PyPNGImage)
+    parser.add_argument("--out_folder", default="images", type=str, help="")
     args = parser.parse_args(argv)
     try:
-        qr_code_generator(args.file, args.box_size, args.border, args.image_factory)
+        qr_code_generator(
+            args.file, args.box_size, args.border, args.image_factory, args.out_folder
+        )
     finally:
         print("=> QR kodlar muvaffaqiyatli generatsiya qilindi!")
 
